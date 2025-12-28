13ad7a3b4516424760efb5dcf285aa38e29735b9
s.toda <s.toda@jin-it.co.jp>
2025-12-28 10:02:22 +0900

    fix:dotenvの追加と読み込み

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,12 +1,8 @@
 import { defineConfig, devices } from '@playwright/test';
+import dotenv from 'dotenv';
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// import dotenv from 'dotenv';
-// import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
+/* rootの.env読み込み */
+dotenv.config();
 
 /**
  * See https://playwright.dev/docs/test-configuration.
@@ -26,7 +22,11 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
-    // baseURL: 'http://localhost:3000',
+    baseURL: process.env.BASE_URL,
+    httpCredentials: {
+      username: process.env.BASIC_AUTH_USER ?? '',
+      password: process.env.BASIC_AUTH_PASSWORD ?? '',
+    },
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
